dee2d737c524be5e4966a7b254da84a48d3fecd5
Orlando1991 <osd_1991@hotmail.com>
2017-05-22 12:00:21 +0200

    stuff

--- User-Item/Program.cs
+++ User-Item/Program.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 
 namespace UserItem
 {
     class Program
     {
-        private static string FILE_PATH = "C:\\Users\\Orlando\\Documents\\HRO\\INF3B\\OP4\\Data Science\\UserItem\\User-Item\\userItem.csv";
+        private static string FILE_PATH = "userItem.csv";
         static Dictionary<int, User> userpref = new Dictionary<int, User>();
         static List<int> uniqueArticles = new List<int>();
         static StrategyInterface ed, pe, co;
@@ -16,31 +17,73 @@ namespace UserItem
         static void Main(string[] args)
         {
             readFile();
-            printUsers();
-            //possible keys 1 .. 7
-            var user1 = userpref[1]; //[2,6] 1,58 en 0,95
-            var amount_nearestNeighbours = 3;
-            nearestNeighbours(user1, amount_nearestNeighbours);
-            
+            // printUsers();
 
+            //possible keys 1 .. 7
+            var user1 = userpref[7]; //[2,6] 1,58 en 0,95
+            var amount_nearestNeighbours = 3;
+            
+            //will return a list of size n with neirest neighbours eventually
+            nearestNeighbours(user1, amount_nearestNeighbours);
         }
-        //will return a list of size n with neirest neighbours eventually
+        //how to take all the similarities into consideration and have method return only one list????
         private static void nearestNeighbours(User selectedUser, int amount_nearestNeighbours)
         {
             //loop through all the users and find the users that are most similar to selecteduser
-            var user2 = userpref[7];
-            ed = new EuclidianDistance();
-            var euclidian_distance = ed.calculate(selectedUser, user2, uniqueArticles);
-            Console.WriteLine("Euclidian stuff:" + euclidian_distance);
+            foreach(var user in userpref)
+            {
+                Dictionary<int, double> nn_Euclidian = new Dictionary<int, double>();
+                Dictionary<int, double> nn_Pearson = new Dictionary<int, double>();
+                Dictionary<int, double> nn_Cosine = new Dictionary<int, double>();
 
-            pe = new Pearsons();
-            var correlation = pe.calculate(selectedUser, user2, uniqueArticles);
-            Console.WriteLine("Pearsons corr:" + correlation);
+                if(selectedUser.id != user.Key)
+                {
+                    Console.WriteLine("Compare user: " + selectedUser.id + " with user: " + user.Key);
+                    Console.WriteLine("---------------------");
+                    var user2 = userpref[user.Key];
+
+                    ed = new EuclidianDistance();
+                    var euclidian_distance = ed.calculate(selectedUser, user2, uniqueArticles);
+                    nn_Euclidian = sortEuclidianNeighbours(user2.id, euclidian_distance, nn_Euclidian, amount_nearestNeighbours);
+                    Console.WriteLine("Euclidian stuff: " + euclidian_distance);
+
+                    pe = new Pearsons();
+                    var correlation = pe.calculate(selectedUser, user2, uniqueArticles);
+                    Console.WriteLine("Pearsons corr: " + correlation);
+                
+                    co = new Cosine();
+                    var cosine_similarity = co.calculate(selectedUser, user2, uniqueArticles);
+                    Console.WriteLine("Cosine similarity: " + cosine_similarity + "\n");
+                } else
+                {
+                    continue;     
+                }
+                
+            }
             
-            co = new Cosine();
-            var cosine_similarity = co.calculate(selectedUser, user2, uniqueArticles);
-            Console.WriteLine("Cosine similarity:" + cosine_similarity);
         }
+
+        private static Dictionary<int, double> sortEuclidianNeighbours(int id, double euclidian_distance, Dictionary<int, double> nearestNeighbours, int amount_nearestNeighbours)
+        {
+            if(nearestNeighbours.Count < amount_nearestNeighbours)
+            {
+                nearestNeighbours.Add(id, euclidian_distance);
+                //sort dictionary here
+            } else
+            {
+                //the first value has the biggest distance, so only this will need to be replaced
+                var first = nearestNeighbours.First();
+                double firstValue = first.Value;
+                if(euclidian_distance < firstValue)
+                {
+                    //remove firstvalue here
+                    //add euclidian_distance here
+                    //sort nearestneighbours
+                }
+            }
+            return nearestNeighbours;           
+        }
+
         private static void readFile()
         {
             using(FileStream fs = File.OpenRead(FILE_PATH))
