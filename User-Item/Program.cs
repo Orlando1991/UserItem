4e7207c544b32673eb5e65dd49d6fde22549782f
Orlando1991 <osd_1991@hotmail.com>
2017-05-22 16:27:53 +0200

    predicted ratings done
    TODO new dataset stuff

--- User-Item/Program.cs
+++ User-Item/Program.cs
@@ -20,29 +20,54 @@ namespace UserItem
             // printUsers();
 
             //possible keys 1 .. 7
-            var user1 = userpref[7]; //[2,6] 1,58 en 0,95
-            var amount_nearestNeighbours = 3;
-<<<<<<< Updated upstream
-            
-            //will return a list of size n with neirest neighbours eventually
-            nearestNeighbours(user1, amount_nearestNeighbours);
-=======
-            nearestNeighbours(user1, amount_nearestNeighbours);
-
-
->>>>>>> Stashed changes
+            var user1 = userpref[4]; 
+            var amount_nearestNeighbours = 3;          
+            var nearest_Neighbours =  nearestNeighbours(user1);
+            predictedRatings(nearest_Neighbours, amount_nearestNeighbours);
         }
-        //how to take all the similarities into consideration and have method return only one list????
-        private static void nearestNeighbours(User selectedUser, int amount_nearestNeighbours)
+
+        private static void predictedRatings(List<User> nearest_Neighbours, int amount_nearestNeighbours)
         {
+            List<double> pearson_values = new List<double>();
+            List<double> weight = new List<double>();
+            double sum = 0;
+            for(var i = 0; i< amount_nearestNeighbours; i++)
+            {
+                var user = nearest_Neighbours[i];
+                var pearson_correlation = user.pearson_correlation;
+                pearson_values.Add(pearson_correlation);
+                sum += pearson_correlation;
+            }
+
+            foreach(var value in pearson_values)
+            {
+                var result = value/sum;
+                weight.Add(result);
+            }
+            //list that containts the articles we want the ratings from from the other users
+            List<int> articleRatings = new List<int>{101};            
+            for(var i = 0; i < articleRatings.Count; i++)
+            {
+                var article = articleRatings[i];
+                var predictedRating = 0.0;
+                for(var j = 0; j < amount_nearestNeighbours; j++)
+                {
+                    var currentNeighbour = nearest_Neighbours[j];
+                    var userRating  = currentNeighbour.articleRating[article];
+                    var weighted_rating = weight[j] * userRating;
+                    predictedRating += weighted_rating;
+                } 
+                Console.WriteLine("Predicted rating for article : " + article + " is: " + predictedRating);                         
+            }
+        } 
+        private static List<User> nearestNeighbours(User selectedUser)
+        {
+            List<User> nn_Euclidian = new List<User>();
+            List<User> nn_Pearson = new List<User>();
+            List<User> nn_Cosine = new List<User>();
             //loop through all the users and find the users that are most similar to selecteduser
             foreach(var user in userpref)
             {
-                Dictionary<int, double> nn_Euclidian = new Dictionary<int, double>();
-                Dictionary<int, double> nn_Pearson = new Dictionary<int, double>();
-                Dictionary<int, double> nn_Cosine = new Dictionary<int, double>();
-
-<<<<<<< Updated upstream
                 if(selectedUser.id != user.Key)
                 {
                     Console.WriteLine("Compare user: " + selectedUser.id + " with user: " + user.Key);
@@ -51,55 +76,42 @@ namespace UserItem
 
                     ed = new EuclidianDistance();
                     var euclidian_distance = ed.calculate(selectedUser, user2, uniqueArticles);
-                    nn_Euclidian = sortEuclidianNeighbours(user2.id, euclidian_distance, nn_Euclidian, amount_nearestNeighbours);
+                    user2.euclidean_distance = euclidian_distance;
+                    nn_Euclidian.Add(user2);
                     Console.WriteLine("Euclidian stuff: " + euclidian_distance);
 
                     pe = new Pearsons();
                     var correlation = pe.calculate(selectedUser, user2, uniqueArticles);
+                    user2.pearson_correlation = correlation;
+                    nn_Pearson.Add(user2);
+
                     Console.WriteLine("Pearsons corr: " + correlation);
                 
                     co = new Cosine();
                     var cosine_similarity = co.calculate(selectedUser, user2, uniqueArticles);
+                    user2.cosine = cosine_similarity;
+                    nn_Cosine.Add(user2);
+
                     Console.WriteLine("Cosine similarity: " + cosine_similarity + "\n");
                 } else
                 {
                     continue;     
-                }
-                
+                }      
             }
-            
-=======
-            pe = new Pearsons();
-            var correlation = pe.calculate(selectedUser, user2, uniqueArticles);
-            Console.WriteLine("Pearsons corr:" + correlation);
+            //maybe print the first 3 of this stuff
+            List<User> nn_Euclidian_Sorted = nn_Euclidian.OrderBy(x => x.euclidean_distance).ToList();
+            List<User> nn_Pearson_Sorted = nn_Pearson.OrderByDescending(x => x.pearson_correlation).ToList();
+            List<User> nn_Cosine_Sorted = nn_Cosine.OrderByDescending(x => x.cosine).ToList();
 
-            co = new Cosine();
-            var cosine_similarity = co.calculate(selectedUser, user2, uniqueArticles);
-            Console.WriteLine("Cosine similarity:" + cosine_similarity);
->>>>>>> Stashed changes
-        }
-
-        private static Dictionary<int, double> sortEuclidianNeighbours(int id, double euclidian_distance, Dictionary<int, double> nearestNeighbours, int amount_nearestNeighbours)
-        {
-            if(nearestNeighbours.Count < amount_nearestNeighbours)
+            for(var i =0; i < nn_Euclidian_Sorted.Count; i++)
             {
-                nearestNeighbours.Add(id, euclidian_distance);
-                //sort dictionary here
-            } else
-            {
-                //the first value has the biggest distance, so only this will need to be replaced
-                var first = nearestNeighbours.First();
-                double firstValue = first.Value;
-                if(euclidian_distance < firstValue)
-                {
-                    //remove firstvalue here
-                    //add euclidian_distance here
-                    //sort nearestneighbours
-                }
+                Console.WriteLine("Euclidian nn: " + nn_Euclidian_Sorted[i].id);
+                Console.WriteLine("Pearson nn: " + nn_Pearson_Sorted[i].id);
+                Console.WriteLine("consine nn: " + nn_Cosine_Sorted[i].id);
             }
-            return nearestNeighbours;           
-        }
 
+            return nn_Pearson_Sorted;
+        }
         private static void readFile()
         {
             using (FileStream fs = File.OpenRead(FILE_PATH))
--- User-Item/User.cs
+++ User-Item/User.cs
@@ -7,6 +7,10 @@ namespace UserItem
     {
         public int id { get; set; }
         public Dictionary<int, double> articleRating { get; set;}
+        public double euclidean_distance {get; set;}
+        public double cosine {get; set;}
+        public double pearson_correlation {get; set;}
+
         public User(int id, Dictionary<int, double> articleRating)
         {
             this.id = id;
