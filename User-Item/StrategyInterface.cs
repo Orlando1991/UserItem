0ac6b16716c34536d4eda398a3edd4e569441fff
Eldin66 <eldin.33@outlook.com>
2017-05-22 12:24:03 +0200

    fix

--- User-Item/StrategyInterface.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace UserItem
-{
-    interface StrategyInterface
-    {
-        double calculate(User user1, User user2, List<int> uniqueArticles);
-    }
-}
